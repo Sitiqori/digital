b52c57a4f65242a2f673beb49160cbdd520ae698
Sitiqori <96257080+Sitiqori@users.noreply.github.com>
2024-02-23 10:45:00 +0700

    Add files via upload
    db nya

---
+++ digital.sql
@@ -0,0 +1,489 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 23, 2024 at 04:44 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `digital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `penulis` varchar(255) NOT NULL,
+  `penerbit` varchar(255) NOT NULL,
+  `tahun_terbit` year(4) NOT NULL,
+  `sinopsis` text DEFAULT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'instock',
+  `cover` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `kategori_id` bigint(20) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id`, `judul`, `slug`, `penulis`, `penerbit`, `tahun_terbit`, `sinopsis`, `status`, `cover`, `created_at`, `updated_at`, `kategori_id`) VALUES
+(2, ' The Prisoner of Azkaban', ' The Prisoner of Azkaban', 'Jk Rowling', 'lorem', '2005', 'Lorem ipsum dolor sit . Voluptatibus quis eaque, reiciendis asperiores non dolorum mollitia consequuntur, distinctio iste laborum odit temporibus cupiditate?\r\n', 'instock', NULL, NULL, NULL, NULL),
+(3, 'The Order of the Phoenix', 'the-order-of-the-phoenix', 'Jk Rowling', 'Gramedia', '2005', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis saepe similique nesciunt quod nam optio alias eaque, explicabo consequuntur repellendus amet neque est, deserunt exercitationem minus. Facere repellat assumenda fugiat!', 'instock', 'C:\\xampp8.2.12-0-VS16\\tmp\\phpA3C2.tmp', '2024-02-22 17:53:47', '2024-02-22 17:53:47', NULL),
+(4, 'The Deathly Hallows', 'the-deathly-hallows', 'Jk Rowling', 'Gramedia', '2005', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis saepe similique nesciunt quod nam optio alias eaque, explicabo consequuntur repellendus amet neque est, deserunt exercitationem minus. Facere repellat assumenda fugiat!', 'instock', 'The Deathly Hallows-1708652967-jpg', '2024-02-22 18:49:28', '2024-02-22 18:49:28', NULL),
+(5, 'The Goblet of Fire', 'the-goblet-of-fire', 'Jk Rowling', 'Gramedia', '2003', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis saepe similique nesciunt quod nam optio alias eaque, explicabo consequuntur repellendus amet neque est, deserunt exercitationem minus. Facere repellat assumenda fugiat!', 'instock', 'The Goblet of Fire-1708653519-jpg', '2024-02-22 18:58:39', '2024-02-22 18:58:39', NULL),
+(6, 'ancuki ancika', 'ancuki-ancika', 'Jk Rowling', 'Gramedia', '2003', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis saepe similique nesciunt quod nam optio alias eaque, explicabo consequuntur repellendus amet neque est, deserunt exercitationem minus. Facere repellat assumenda fugiat!', 'instock', '1708654235-jpeg', '2024-02-22 19:10:36', '2024-02-22 19:10:36', NULL),
+(7, 'Ancuki', 'ancuki', 'Jk Rowling', 'Gramedia', '2003', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis saepe similique nesciunt quod nam optio alias eaque, explicabo consequuntur repellendus amet neque est, deserunt exercitationem minus. Facere repellat assumenda fugiat!', 'instock', '1708654300-.jpeg', '2024-02-22 19:11:40', '2024-02-22 19:11:40', NULL),
+(8, 'Ancuku Ancika Ancaki', 'ancuku-ancika-ancaki', 'Jk Rowling', 'Gramedia', '2003', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis saepe similique nesciunt quod nam optio alias eaque, explicabo consequuntur repellendus amet neque est, deserunt exercitationem minus. Facere repellat assumenda fugiat!', 'instock', 'Ancuku Ancika Ancaki-1708654446-.jfif', '2024-02-22 19:14:06', '2024-02-22 19:14:06', NULL),
+(9, 'cuyung', 'cuyung', 'Jk Rowling', 'Gramedia', '2003', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis saepe similique nesciunt quod nam optio alias eaque, explicabo consequuntur repellendus amet neque est, deserunt exercitationem minus. Facere repellat assumenda fugiat!', 'instock', 'cuyung-1708654502-.jpeg', '2024-02-22 19:15:02', '2024-02-22 19:15:02', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategoribuku`
+--
+
+CREATE TABLE `kategoribuku` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `namakategori` varchar(255) NOT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `kategoribuku`
+--
+
+INSERT INTO `kategoribuku` (`id`, `namakategori`, `slug`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'History', 'history', NULL, '2024-02-21 18:03:16', NULL),
+(3, 'Fiction', 'Fiction', '2024-02-20 01:19:16', '2024-02-20 01:19:16', NULL),
+(4, 'Sport', 'Sport', '2024-02-20 17:58:04', '2024-02-20 17:58:04', NULL),
+(5, 'Triller', 'Triller', '2024-02-20 17:58:53', '2024-02-21 20:59:23', NULL),
+(8, 'Family', 'family', '2024-02-21 19:09:27', '2024-02-21 19:09:27', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategoribuku_relasi`
+--
+
+CREATE TABLE `kategoribuku_relasi` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `buku_id` bigint(20) UNSIGNED NOT NULL,
+  `kategori_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `koleksipribadi`
+--
+
+CREATE TABLE `koleksipribadi` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `buku_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2024_02_07_050636_create_buku_table', 1),
+(6, '2024_02_07_050940_create_ulasan_buku_table', 1),
+(7, '2024_02_07_050956_create_koleksipribadi_table', 1),
+(8, '2024_02_07_051019_create_peminjaman_table', 1),
+(9, '2024_02_07_051037_create_kategoribuku_table', 1),
+(10, '2024_02_07_051048_create_kategoribuku_relasi_table', 1),
+(11, '2024_02_07_051135_create_roles_table', 1),
+(12, '2024_02_07_051218_add_kategori_id_to_buku_table', 1),
+(13, '2024_02_07_052055_add_role_id_column_to_users_table', 2),
+(14, '2024_02_21_012150_add_slug_column_to_kategoribuku_table', 3),
+(15, '2024_02_21_013606_change_slug_column_into_nullable_in_kategoribuku_table', 4),
+(16, '2024_02_22_010649_add_soft_delete_column_to_kategoribuku_table', 5),
+(17, '2024_02_22_040354_add_slug_cover_column_to_buku_table', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `buku_id` bigint(20) UNSIGNED NOT NULL,
+  `TanggalPeminjaman` date NOT NULL,
+  `TanggalPengembalian` date NOT NULL,
+  `TanggalHarusDikembalikan` date NOT NULL,
+  `StatusPeminjaman` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'admin', NULL, NULL),
+(2, 'petugas', NULL, NULL),
+(3, 'peminjam', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ulasan_buku`
+--
+
+CREATE TABLE `ulasan_buku` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `buku_id` bigint(20) UNSIGNED NOT NULL,
+  `ulasan` text NOT NULL,
+  `rating` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `nama_lengkap` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'active',
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `role_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `email_verified_at`, `nama_lengkap`, `alamat`, `status`, `remember_token`, `created_at`, `updated_at`, `role_id`) VALUES
+(1, 'admin1', '$2y$10$wK1FqURLvUgLEaP1oAETOOQEp/APDFNXY51sZCR4HfxPo.jDoBBOe', 'admin1@gmail.com', NULL, 'Admin Ke 1', 'alamat Admin', 'active', NULL, NULL, NULL, 1),
+(2, 'petugas1', '$2y$10$wK1FqURLvUgLEaP1oAETOOQEp/APDFNXY51sZCR4HfxPo.jDoBBOe', 'petugas1@gmail.com', NULL, 'Petugas Ke 1', 'alamat petugas 1', 'active', NULL, NULL, NULL, 2),
+(3, 'peminjam1', '$2y$10$wK1FqURLvUgLEaP1oAETOOQEp/APDFNXY51sZCR4HfxPo.jDoBBOe', 'peminjam1@gmail.com', NULL, 'Peminjam ke 1', 'alamat peminjam no 1', 'active', NULL, NULL, NULL, 3),
+(5, 'coba1', '$2y$12$Px/qB4JgW7Mc1brgjtqPCeVgQZHTTtSP7WKtctmKbGIsoJWcF6bj2', 'cobapertama@gmail.com', NULL, 'coba pertama', 'alamat coba', 'active', NULL, '2024-02-19 18:28:07', '2024-02-19 18:28:07', 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `buku_kategori_id_foreign` (`kategori_id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `kategoribuku`
+--
+ALTER TABLE `kategoribuku`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kategoribuku_relasi`
+--
+ALTER TABLE `kategoribuku_relasi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kategoribuku_relasi_buku_id_foreign` (`buku_id`),
+  ADD KEY `kategoribuku_relasi_kategori_id_foreign` (`kategori_id`);
+
+--
+-- Indexes for table `koleksipribadi`
+--
+ALTER TABLE `koleksipribadi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `koleksipribadi_user_id_foreign` (`user_id`),
+  ADD KEY `koleksipribadi_buku_id_foreign` (`buku_id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `peminjaman_user_id_foreign` (`user_id`),
+  ADD KEY `peminjaman_buku_id_foreign` (`buku_id`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ulasan_buku`
+--
+ALTER TABLE `ulasan_buku`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ulasan_buku_user_id_foreign` (`user_id`),
+  ADD KEY `ulasan_buku_buku_id_foreign` (`buku_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD KEY `users_role_id_foreign` (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kategoribuku`
+--
+ALTER TABLE `kategoribuku`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `kategoribuku_relasi`
+--
+ALTER TABLE `kategoribuku_relasi`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `koleksipribadi`
+--
+ALTER TABLE `koleksipribadi`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `ulasan_buku`
+--
+ALTER TABLE `ulasan_buku`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `buku`
+--
+ALTER TABLE `buku`
+  ADD CONSTRAINT `buku_kategori_id_foreign` FOREIGN KEY (`kategori_id`) REFERENCES `kategoribuku` (`id`);
+
+--
+-- Constraints for table `kategoribuku_relasi`
+--
+ALTER TABLE `kategoribuku_relasi`
+  ADD CONSTRAINT `kategoribuku_relasi_buku_id_foreign` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`id`),
+  ADD CONSTRAINT `kategoribuku_relasi_kategori_id_foreign` FOREIGN KEY (`kategori_id`) REFERENCES `kategoribuku` (`id`);
+
+--
+-- Constraints for table `koleksipribadi`
+--
+ALTER TABLE `koleksipribadi`
+  ADD CONSTRAINT `koleksipribadi_buku_id_foreign` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`id`),
+  ADD CONSTRAINT `koleksipribadi_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `peminjaman_buku_id_foreign` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`id`),
+  ADD CONSTRAINT `peminjaman_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `ulasan_buku`
+--
+ALTER TABLE `ulasan_buku`
+  ADD CONSTRAINT `ulasan_buku_buku_id_foreign` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`id`),
+  ADD CONSTRAINT `ulasan_buku_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
